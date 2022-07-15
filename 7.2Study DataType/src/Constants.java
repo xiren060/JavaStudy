af1882055bad2d0e232fc5341fc469d8d3f0014c
xiren060 <95229225+xiren060@users.noreply.github.com>
2022-07-15 22:42:37 +0800

    Add files via upload

---
+++ 7.2Study DataType/src/Constants.java
@@ -0,0 +1,9 @@
+public class Constants {
+    public static void main(String[] args) {
+        final double CM_PER_INCH=2.54;
+        double paperWidth=8.5;
+        double paperHeigh=11;
+        System.out.println("paper size: " + paperHeigh*CM_PER_INCH + "by" + paperWidth*CM_PER_INCH);
+
+    }
+}
